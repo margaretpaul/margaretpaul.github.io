021bdac62b867bee52904bf64ed1cad94135c386
margaretpaul <mapa3269@colorado.edu>
2023-03-23 00:38:40 -0600

    update index

--- css-midterm/index.js
+++ css-midterm/index.js
@@ -24,3 +24,31 @@ for (i = 0; i < coll.length; i++) {
     } 
   });
 }
+
+
+var coll = document.getElementsByClassName("collapsible2");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
