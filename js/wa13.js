7043b1519086d9c844c02c802341953d0967d1b8
margaretpaul <mapa3269@colorado.edu>
2023-04-20 11:09:12 -0600

    wa13

--- js/wa13.js
+++ js/wa13.js
@@ -13,22 +13,6 @@ function reset() {
    output.textContent = outputInt;
 }
 
-// function minus() {
-   // if (outputInt > 0) {
-   // outputInt -=1;
-   // output.textContent = outputInt; }
-    
-//}
-
-//function plus() {
- //   if (outputInt < 100) {
- //   outputInt +=1;
- //  output.textContent = outputInt;
- //  }
- //  if( e.keyCode == 32 ) {
-  //  addHit();
- // }
-//}
 
 var hits = 0;
 var hitElement = document.querySelector( '.hits' );
@@ -51,54 +35,17 @@ var renderHits = function() {
  hitElement.innerHTML = hits;
 }
 
-//var resetHits = function() {
- // hits = 0;
- // renderHits();
-//}
-
-//function random() {
-   // outputInt = randomNumber(0, 100);
-   // output.textContent = outputInt;
-//}
-
-//function randomNumber(min, max) {
-  //  const num = Math.floor(Math.random() * (max - min + 1)) + min;
-  //  return num;
- // }
-
-
-
 const output = document.querySelector('.output');
 let outputInt = parseInt(output.textContent);
 console.log(outputInt);
 
-//const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
-//const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
+
 const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
-//const randomButton = document.querySelector('.random-button').addEventListener('click', random);
+
 const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
 
 
-/* const button = document.querySelector('.button');
-const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
-button.addEventListener('click', updateOutput);
-function updateOutput() {
-    output.textContent = phone_content.value;
-    alert(phone_content.value);
-}
-*/
 
-
-// var slider = document.getElementById("myRange");
-// var sliderSubmit = document.querySelector(".slider-submit-button").addEventListener('click', update);
-// var sliderOutput = document.querySelector(".slider-output");
-
-
-// Update the current slider value (each time you drag the slider handle)
-// function update() {
-  // sliderOutput.textContent = slider.value;
-// }
 
 
 
