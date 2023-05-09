377df9dc992ac57bbb5a769165ab524aaf2df0fa
margaretpaul <mapa3269@colorado.edu>
2023-05-09 13:33:20 -0600

    js midterm

--- js-midterm/js-midterm.js
+++ js-midterm/js-midterm.js
@@ -1,47 +1,79 @@
-const words = [ 'banana', 'yellow','people','coffee','purple','summer','guitar','jacket','friend','beauty','purple','cookie','rocket','bottle','camera','church','pencil','window','tongue','school','silver','orange','dragon','rocket','basket'];
+const words = [
+  "colorado",
+  "boulder",
+  "flagstaff",
+  "buffalos",
+  "ralphie",
+  "sko",
+  "flatirons",
+  "chautauqua",
+  "pearl street",
+  "folsom field",
+  "buffs",
+  "the hill",
+  "rocky mountains",
+  "the fox",
+  "boulder theater",
+];
+
 const word = words[Math.floor(Math.random() * words.length)];
+const guessedLetters = [];
+let incorrectGuesses = 0;
 
-let guesses = 0;
-let correctGuesses = 0;
-let answerArray = [];
-for (let i = 0; i < word.length; i++) {
-  answerArray[i] = '-';
-}
+function displayWord() {
+  let displayedWord = "";
 
-function guessLetter() {
-    const guess = document.getElementById('input-word').value;
-    if (guess.length !== 6) {
-        alert('enter a 6 letter word');
-      return;
-
-  }
-  guesses++;
-  let correct = false;
-  for (let i = 0; i < word.length; i++) {
-    if (word[i] === guess[i]) {
-      answerArray[i] = guess[i];
-      correctGuesses++;
-      correct = true;
+  for (const letter of word) {
+    if (guessedLetters.includes(letter)) {
+      displayedWord += letter;
+    } else {
+      displayedWord += "_";
     }
   }
-  const result = document.getElementById('result-container');
-  result.textContent = answerArray.join(' ');
-  if (correctGuesses === word.length) {
-    result.textContent = `Correct! You guessed "${word}" in ${guesses} guesses`;
-   
+
+  document.getElementById("word").textContent = displayedWord;
+}
+
+function handleKeyPress(event) {
+  if (/^[a-zA-Z ]$/.test(event.key)) {
+    const letter = event.key.toLowerCase();
+    if (!guessedLetters.includes(letter)) {
+      guessedLetters.push(letter);
+
+      if (word.includes(letter)) {
+        document.getElementById("guesses").textContent = `You guessed "${letter}" correctly!`;
+
+        displayWord();
+        if (!document.getElementById("word").textContent.includes("_")) {
+          document.getElementById("guesses").textContent = "Thats it!";
+          
+          document.removeEventListener("keydown", handleKeyPress);
+        }
+      } else {
+        incorrectGuesses++;
+
+        document.getElementById("guesses").textContent = `You guessed "${letter}" incorrectly! ${incorrectGuesses}/10`;
+
+        if (incorrectGuesses === 10) {
+          document.getElementById("guesses").textContent = `Darn! The word was "${word}".`;
+          document.removeEventListener("keydown", handleKeyPress);
+        }
+      }
+    }
   }
 }
 
-// Event listener for the submit button
-document.getElementById('submit-button').addEventListener('click', guessLetter);
+displayWord();
+document.addEventListener("keydown", handleKeyPress);
+
 
 const coll = document.querySelector(".collapsible");
 coll.addEventListener("click", function() {
-  this.classList.toggle("active");
-  const content = this.nextElementSibling;
-  if (content.style.maxHeight) {
-    content.style.maxHeight = null;
-  } else {
-    content.style.maxHeight = content.scrollHeight + "px";
-  }
+this.classList.toggle("active");
+const content = this.nextElementSibling;
+if (content.style.maxHeight) {
+  content.style.maxHeight = null;
+} else {
+  content.style.maxHeight = content.scrollHeight + "px";
+}
 });
